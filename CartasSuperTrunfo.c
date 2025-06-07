53a703e61ba89b55c03fa1238319aef4005e36a9
betyzi <b3atrizmach4do09@gmail.com>
2025-06-07 15:23:42 +0000

    Novo commit, desfio nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-
-    //Primeiro começo definindo as veriáveis do codigo para armazenar os dados das cartas.
+    
+//Primeiro começo definindo as veriáveis do codigo para armazenar os dados das cartas.
 
     // Variáveis para a Carta 1
     char estado1;
@@ -11,7 +11,7 @@ int main() {
     unsigned long int populacao1;
     float area1, pib1;
     int pontosturisticos1;
-    float densidade1, pibpercapita1;
+    float densidade1, pibpercapita1, superpoder1;
 
     // Variáveis para a Carta 2
     char estado2;
@@ -20,10 +20,9 @@ int main() {
     unsigned long int populacao2;
     float area2, pib2;
     int pontosturisticos2;
-    float densidade2, pibpercapita2;
+    float densidade2, pibpercapita2, superpoder2;
 
-// Depois uso as funções certas para a entrada de dados das cartas.
-
+// Depo,is uso as funções certas para a entrada de dados das cartas.
 
     // Pegando os ndados da carata 1
     printf("Cadastro da Carta 1\n");
@@ -42,7 +41,7 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosturisticos1);
 
-    //Pegando os ndados da carata 2
+    // Coletando dados da Carta 2
     printf("\nCadastro da Carta 2\n");
     printf("Digite o estado (A-H): ");
     scanf(" %c", &estado2);
@@ -61,41 +60,30 @@ int main() {
 
 //Agora uso as funções para calcular a densidade e o pib per capta das cartas 
 
-   // Cálculos
+    // Cálculando 
     densidade1 = populacao1 / area1;
     pibpercapita1 = pib1 / populacao1;
+    superpoder1 = (float)populacao1 + area1 + pib1 + pontosturisticos1 + pibpercapita1 + (1 / densidade1);
 
     densidade2 = populacao2 / area2;
     pibpercapita2 = pib2 / populacao2;
+    superpoder2 = (float)populacao2 + area2 + pib2 + pontosturisticos2 + pibpercapita2 + (1 / densidade2);
 
-//Por fim, exibo a informações e o resultado dos calculoas no terminal
+//Agora faço as comparações das informações das cartas e exibo os resultados de qual ganhou em cada requisito
 
-    // Exibindo as cartas
-    printf("\n--- Carta 1 ---\n");
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomecidade1);
-    printf("População: %lu\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Pontos Turísticos: %d\n", pontosturisticos1);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
-    printf("PIB per Capita: %.2f reais\n", pibpercapita1);
-   
-    printf("\n--- Carta 2 ---\n");
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nomecidade2);
-    printf("População: %lu\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Pontos Turísticos: %d\n", pontosturisticos2);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
-    printf("PIB per Capita: %.2f reais\n", pibpercapita2);
-    
-    printf("Novo comit\n");
+     // Comparações e resultados
+    printf("\n--- Resultados das Comparações ---\n");
+
+    printf("População: %s\n", populacao1 > populacao2 ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+    printf("Área: %s\n", area1 > area2? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+    printf("PIB: %s\n", pib1 > pib2 ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+    printf("Pontos Turísticos: %s\n", pontosturisticos1 > pontosturisticos2 ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+    printf("Densidade Populacional: %s\n", densidade1 < densidade2 ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+    printf("PIB per Capita: %s\n", pibpercapita1 > pibpercapita2 ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+    printf("Super Poder: %s\n", superpoder1 > superpoder2 ? "Carta 1 venceu (1)" : "Carta 2 venceu (0)");
+
+    printf("Novo commit");
+    printf("Novo commit3");
 
     return 0;
 }
-    // Obs: Sempre devo deixar o código organizado para facilitar a compreenção do código e arrumar possíveis erros.
-
